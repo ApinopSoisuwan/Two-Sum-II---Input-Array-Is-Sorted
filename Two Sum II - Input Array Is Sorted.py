092b2d57c18cf509f4745ab3f10969cbf01bb1ed
Apinop <apinop.soisuwan@gmail.com>
2021-11-25 21:46:23 +0700

    short v.2

--- Two Sum II - Input Array Is Sorted.py
+++ Two Sum II - Input Array Is Sorted.py
@@ -1,17 +1,21 @@
 def twoSum(numbers,target):
-    l,h = 0,len(numbers) -1
-    answer = []
-    while l < h :
+    l = 0
+    h = len(numbers) -1
+    re = []
+    l, h = 0, len(numbers) - 1
+    while l < h:
         if numbers[l] + numbers[h] == target:
-            answer.append(l+1)
-            answer.append(h+1)
-        if numbers[l] + numbers[h] > target :
+            return l + 1, h + 1
+        if numbers[l] + numbers[h] > target:
             h -= 1
         else:
             l += 1
-    return answer
+"""
+Runtime: 87 ms, faster than 26.03% of Python3 online submissions for Two Sum II - Input Array Is Sorted.
+Memory Usage: 14.8 MB, less than 5.38% of Python3 online submissions for Two Sum II - Input Array Is Sorted.
+"""
 
-a1 =[2, 7, 11, 15]
+a1 = [2, 7, 11, 15]
 a2 = 9
 #Output: [1, 2]
 b1 = [2,3,4]
@@ -20,6 +24,7 @@ b2 = 6
 c1 = [-1,0]
 c2 = -1
 #Output: [1,2]
-print(twoSum(a1,a2))
+
 print(twoSum(b1,b2))
 print(twoSum(c1,c2))
+print(twoSum(a1,a2))
